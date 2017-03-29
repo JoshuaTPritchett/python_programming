d6c828b7718fd76d3aa3bdf229329c5b51307bd0
JoshuaTPritchett <jpritchett757@gmail.com>
2017-03-29 10:11:24 -0400

    get some basic shit going

---
+++ basics/basic.py
@@ -0,0 +1,9 @@
+print "Hello World"
+print 3
+print 12/7
+print 12.0/7 #will result in an integer
+print float(12) / 7 #lets cast that shit to float
+print 3 + (2*4) #order of operations sure
+#_ #stores last value?
+#_ * 3 #what is this wizardry..
+
---
+++ basics/internals.py
@@ -0,0 +1,7 @@
+"""
+Holy shit python can compile code?
+Source code -> byte code -> runs in Virtual Machine PVM
+"""
+import py_compile
+py_compile.compile('basic.py')
+
