0d64a629eb0bb8778f09300fc8a6ec7660c8856f
JoshuaTPritchett <jpritchett757@gmail.com>
2017-04-03 16:38:51 -0400

    algo test added

---
+++ algos/merge_sort.py
@@ -0,0 +1,26 @@
+"""
+    With this I will code merge sort
+"""
+
+# Merge procedure should take
+# p = low
+# q = mid
+# r = high
+# Each are indicies
+# Assumes that subarray A[p..q] and A[q+1..r] are sorted
+# Will merge these two arrays to form new subarry A[p..r]
+# Takes O(n) where n = high - low + 1 -- total number of elements merged
+# Two piles of cards
+#   Merge two piles
+#   Each pile is sorted smallest on top
+#   Merge to single output array
+#   Choose smaller of two cards on top and place it in new pile
+#   Repeat until one pile is empty then just fill remaining
+
+def merge():
+
+
+def start():
+    # Attempt to handle negative numbers
+    a = [1, 5, 6, 7, 8, 9, 2, 3, -1]
+
